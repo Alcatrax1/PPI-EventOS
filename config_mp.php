a04dfc8ea393afb18c29f0ad8f289e0a4fb5b447
Alcatrax1 <pedro938493@gmail.com>
2025-11-27 00:29:33 -0300

    Removido o acess token

--- config_mp.php
+++ config_mp.php
@@ -1,6 +1,6 @@
 <?php
 
-define('MP_ACCESS_TOKEN', 'APP_USR-235438132655667-112519-78a4dfa61667d81e8e92f0e1b8b25608-1288834278');
+define('MP_ACCESS_TOKEN', 'Acess token');
 
 date_default_timezone_set('America/Sao_Paulo');
 ?>  
